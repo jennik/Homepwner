f02d597fe7143ea3404e75d09c436e6bff1b2f64
jennik <evg@jennik.ru>
2014-02-17 23:59:39 +0400

    Initial Commit

---
+++ Homepwner/HPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HPAppDelegate.h
+//  Homepwner
+//
+//  Created by evg on 17.02.14.
+//  Copyright (c) 2014 jennik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Homepwner/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Homepwner
+//
+//  Created by evg on 17.02.14.
+//  Copyright (c) 2014 jennik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HPAppDelegate class]));
+    }
+}
